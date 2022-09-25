dff82ceb1cd635413619568677fc1926c63acf16
JavGog <javierggjavier@gmail.com>
2022-09-24 21:49:30 -0700

    Account class set up, ready to go.

--- src/main/java/com/example/demo/components/DataLoader.java
+++ src/main/java/com/example/demo/components/DataLoader.java
@@ -1,11 +1,22 @@
 package com.example.demo.components;
 
+import com.example.demo.repositories.AccountRepository;
+import com.example.demo.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 
 public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    AccountRepository accountRepository;
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public DataLoader(){}
     @Override
     public void run(ApplicationArguments args) throws Exception {
+
 
     }
 }
--- src/main/java/com/example/demo/models/Account.java
+++ src/main/java/com/example/demo/models/Account.java
@@ -1,8 +1,60 @@
 package com.example.demo.models;
-
 import com.example.demo.repositories.AccountRepository;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "accounts")
 public class Account implements AccountRepository {
-    private int number;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "number")
+    private long number;
+
+    @Column(name = "type")
+    private String type;
+    @JsonIgnoreProperties({"course"})
+    @OneToMany(mappedBy = "course", fetch = FetchType.LAZY)
+    private List<Account> accounts;
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setNumber(long number) {
+        this.number = number;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public long getNumber() {
+        return number;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Account(long number, String type) {
+        this.number = number;
+        this.type = type;
+        this.accounts = new ArrayList<>();
+    }
+    public Account(){}
 }
